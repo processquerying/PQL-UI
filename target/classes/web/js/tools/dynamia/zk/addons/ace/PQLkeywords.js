0cbc348098447ddbb45b3d31435b7fa038b6ee28
Yiliyaer <y.yilihamu@leonardo.com.au>
2019-02-20 16:11:03 +1100

    cleaned up, fixed a bug in wpd file importing a script multiple times

--- target/classes/web/js/tools/dynamia/zk/addons/ace/PQLkeywords.js
+++
@@ -1,4 +0,0 @@
-            var e = "SELECT|INSERT|INTO|FROM|WHERE|EQUALS|OVERLAPS|WITH|SUBSET|PROPER|GetTasks|NOT|AND|OR|ANY|EACH|ALL",
-                t = "TRUE|FALSE",
-                n = "AVG|COUNT|MIN|MAX",
-                c = "CanOccur|AlwaysOccurs|Executes|Canconflict|CanCooccur|Conflict|Cooccur|TotalCasual|TotalConcurrent";
